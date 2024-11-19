dcd1dcebccac9b49e5965d913468ecf4ca52a05a
Your name <arieftaufikrahman123@gmail.com>
2024-11-19 10:49:37 +0700

    Fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   FaInstagram,
   FaFacebookF,
@@ -9,134 +8,127 @@ import {
   FaMapMarkerAlt,
 } from "react-icons/fa";
 
-const Footer = () => (
-  <footer className="w-full text-gray-300 bg-gray-800">
-    <div className="px-6 py-8 mx-auto max-w-screen-2xl">
-      {/* Row 1 */}
-      <div className="grid grid-cols-1 gap-6 pb-8 border-gray-700 md:grid-cols-2 lg:grid-cols-4">
-        {/* Column 1: Logo and About */}
-        <div>
-          <img
-            src="https://gemacendekia.co.id/wp-content/uploads/2022/12/master_logo_gcg-removebg-preview.png"
-            alt="Logo"
-            className="h-8"
-          />
-          <p className="mt-2">
-            Gema Cendekia Gemilang mengembangkan aplikasi ERP (Enterprise
-            Resource Planning) yang inovatif, dengan fitur yang mencakup
-            manajemen keuangan, operasional, logistik dan sumber daya manusia.
-          </p>
+const Footer = () => {
+  const contactLinks = [
+    {
+      href: "https://api.whatsapp.com/send?phone=6281511001239&text=Halo%2C%20saya%20ingin%20mengetahui%20lebih%20lanjut%20tentang%20GCG%20Salus",
+      text: "WhatsApp : +6281511001239",
+      icon: <FaWhatsapp className="text-gray-800" />,
+    },
+    {
+      href: "https://api.whatsapp.com/send?phone=622138741038&text=Halo%2C%20saya%20ingin%20mengetahui%20lebih%20lanjut%20tentang%20GCG%20Salus",
+      text: "WhatsApp : +622138741038",
+      icon: <FaWhatsapp className="text-gray-800" />,
+    },
+    {
+      href: "mailto:sales@gemacendekia.co.id",
+      text: "Email : sales@gemacendekia.co.id",
+      icon: <FaEnvelope className="text-gray-800" />,
+    },
+  ];
 
-          {/* Social Media Icons */}
-          <div className="flex mt-4 space-x-2">
+  const socialMediaLinks = [
+    {
+      href: "https://www.instagram.com/gcg.id",
+      icon: <FaInstagram className="text-gray-800" />,
+    },
+    {
+      href: "https://www.facebook.com/",
+      icon: <FaFacebookF className="text-gray-800" />,
+    },
+    {
+      href: "https://www.youtube.com/",
+      icon: <FaYoutube className="text-gray-800" />,
+    },
+    {
+      href: "https://www.tiktok.com/",
+      icon: <FaTiktok className="text-gray-800" />,
+    },
+  ];
+
+  return (
+    <footer className="w-full text-gray-300 bg-gray-800">
+      <div className="px-6 py-8 mx-auto max-w-screen-2xl">
+        {/* Row 1 */}
+        <div className="grid grid-cols-1 gap-6 pb-8 border-gray-700 md:grid-cols-2 lg:grid-cols-3">
+          {/* Column 1: Logo and About */}
+          <div>
+            <img
+              src="https://gemacendekia.co.id/wp-content/uploads/2022/12/master_logo_gcg-removebg-preview.png"
+              alt="Gema Cendekia Gemilang Logo"
+              className="h-8"
+            />
+            <p className="mt-2">
+              Gema Cendekia Gemilang mengembangkan aplikasi ERP (Enterprise
+              Resource Planning) yang inovatif, dengan fitur yang mencakup
+              manajemen keuangan, operasional, logistik, dan sumber daya
+              manusia.
+            </p>
+
+            {/* Social Media Icons */}
+            <div className="flex mt-4 space-x-2">
+              {socialMediaLinks.map(({ href, icon }) => (
+                <a
+                  key={href}
+                  href={href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="p-2 bg-white rounded-full"
+                >
+                  {icon}
+                </a>
+              ))}
+            </div>
+          </div>
+
+          {/* Column 2: Contact Us */}
+          <div>
+            <h2 className="text-xl font-semibold text-white">Contact Us</h2>
+            {contactLinks.map(({ href, text, icon }) => (
+              <a
+                key={href}
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="block mt-4"
+              >
+                <div className="flex items-center">
+                  <span className="p-2 mr-2 bg-white rounded-full">{icon}</span>
+                  <p>{text}</p>
+                </div>
+              </a>
+            ))}
+          </div>
+
+          {/* Column 3: Address */}
+          <div>
             <a
-              href="https://www.instagram.com/gcg.id"
+              href="https://maps.app.goo.gl/FAUg3EfpUJ4zT6si6"
               target="_blank"
               rel="noopener noreferrer"
-              className="p-2 bg-white rounded-full"
             >
-              <FaInstagram className="text-gray-800" />
-            </a>
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-2 bg-white rounded-full"
-            >
-              <FaFacebookF className="text-gray-800" />
-            </a>
-            <a
-              href="https://www.youtube.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-2 bg-white rounded-full"
-            >
-              <FaYoutube className="text-gray-800" />
-            </a>
-            <a
-              href="https://www.tiktok.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="p-2 bg-white rounded-full"
-            >
-              <FaTiktok className="text-gray-800" />
+              <h2 className="text-xl font-semibold text-white">Address</h2>
+              <div className="flex items-center mt-4">
+                <span className="p-2 mr-2 bg-white rounded-full">
+                  <FaMapMarkerAlt className="text-gray-800" />
+                </span>
+                <p>
+                  Graha Cibinong . JL. Raya Jakarta - Bogor Km 43 Blok H No. 08,
+                  Cirimekar, Kec. Cibinong, Kab. Bogor, Jawa Barat 16917
+                </p>
+              </div>
             </a>
           </div>
         </div>
 
-        {/* Column 2: Contact Us */}
-        <div>
-          <h2 className="text-xl font-semibold text-white">Contact Us</h2>
-          <a
-            href="https://api.whatsapp.com/send?phone=6281511001239&text=Halo%2C%20saya%20ingin%20mengetahui%20lebih%20lanjut%20tentang%20GCG%20Salus"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex items-center mt-4">
-              <span className="p-2 mr-2 bg-white rounded-full">
-                <FaWhatsapp className="text-gray-800" />
-              </span>
-              <p>WhatsApp : +6281511001239</p>
-            </div>
-          </a>
-          <a
-            href="https://api.whatsapp.com/send?phone=622138741038&text=Halo%2C%20saya%20ingin%20mengetahui%20lebih%20lanjut%20tentang%20GCG%20Salus"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className="flex items-center mt-4">
-              <span className="p-2 mr-2 bg-white rounded-full">
-                <FaWhatsapp className="text-gray-800" />
-              </span>
-              <p>WhatsApp : +622138741038</p>
-            </div>
-          </a>
-          <div className="flex items-center mt-4">
-            <span className="p-2 mr-2 bg-white rounded-full">
-              <FaEnvelope className="text-gray-800" />
-            </span>
-            <p>Email : sales@gemacendekia.co.id</p>
-          </div>
-        </div>
-
-        {/* Column 3: Address */}
-        <div>
-          <a
-            href="https://maps.app.goo.gl/FAUg3EfpUJ4zT6si6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className="text-xl font-semibold text-white">Address</h2>
-            <div className="flex items-center mt-4">
-              <span className="p-2 mr-2 bg-white rounded-full">
-                <FaMapMarkerAlt className="text-gray-800" />
-              </span>
-              <p>
-                Graha Cibinong . JL. Raya Jakarta - Bogor Km 43 Blok H No. 08,
-                Cirimekar, Kec. Cibinong, Kab. Bogor, Jawa Barat 16917
-              </p>
-            </div>
-          </a>
-        </div>
-
-        {/* Column 4: Recent Posts */}
-        <div>
-          <h2 className="text-xl font-semibold text-white">Recent Posts</h2>
-          <ul className="mt-4 space-y-2">
-            <li>Our Latest Initiatives</li>
-            <li>Community Outreach Events</li>
-            <li>Upcoming Programs</li>
-          </ul>
+        {/* Row 2 */}
+        <div className="py-3 mt-4 text-sm text-center text-gray-400 border-t border-gray-700">
+          &copy; {new Date().getFullYear()} Gema Cendekia Gemilang. All rights
+          reserved. Created by Arief Taufik Rahman.
         </div>
       </div>
-
-      {/* Row 2 */}
-      <div className="py-3 mt-4 text-sm text-center text-gray-400 border-t border-gray-700">
-        &copy; {new Date().getFullYear()} Gema Cendekia Gemilang. All rights
-        reserved. Created by Arief Taufik Rahman.
-      </div>
-    </div>
-  </footer>
-);
+    </footer>
+  );
+};
 
 export default Footer;
