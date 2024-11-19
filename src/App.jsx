d203a2be91318b723afb3f1ef4edb087ed090253
Your name <arieftaufikrahman123@gmail.com>
2024-11-19 14:45:32 +0700

    Add not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import NotFoundPage from "./pages/NotFoundPage"; // import 404 page
 
 const App = () => (
-  <div className="flex-grow">
-    <Header />
-    <LandingPage />
-    <Footer />
-  </div>
+  <Router>
+    <div className="flex-grow">
+      <Header />
+      <Routes>
+        {/* Define your existing routes */}
+        <Route path="/" element={<LandingPage />} />
+        {/* Catch-all Route for 404 */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  </Router>
 );
 
 export default App;
